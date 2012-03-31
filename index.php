bbd9e89a717c8559da7df3a5f1c5541a74f3ecfa
Tomas Lieberkind <t.lieberkind@gmail.com>
2012-03-31 22:22:54 +0200

    First commut

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+</head>
+<body class="bp">
+	<div class="container">
+
+		<h1 class="title">Club 101</h1>
+		<p class="sub-title">En club sejere</p>
+
+		<div class="progress-bar">
+			<div class="progress-bar-inside"></div>
+			<p class="timer-seconds">53</p>
+		</div>
+
+		<div class="club-number">Club #53</div>
+		<div class="current-song">Malk De Koijn - Ørkenstorm</div>
+
+	</div>
+</body>
+</html>
